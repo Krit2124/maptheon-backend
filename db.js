8511c4384a978429451cd542410f0b4155c1cd11
Krit2124 <kritserg2124@gmail.com>
2024-06-08 21:39:36 +0300

    Подготовка для vercel 3

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const {Sequelize} = require("sequelize")
 
-const sequelize = new Sequelize("maptheonDB", "root", "", {host: "localhost", dialect: "mysql"})
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_LOGIN, process.env.DB_PASSWORD, {host: process.env.DB_HOST, dialect: "mysql"})
 
 try {
     sequelize.authenticate();
