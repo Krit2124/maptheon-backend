b949b8c18ba0fa69e535e024c834b3691085f903
Krit2124 <kritserg2124@gmail.com>
2024-06-09 19:35:07 +0300

    Тест 3

--- db.js
+++ db.js
@@ -1,6 +1,10 @@
 const {Sequelize} = require("sequelize")
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_LOGIN, process.env.DB_PASSWORD, {host: process.env.DB_HOST, dialect: "mysql"})
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_LOGIN, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST, 
+    dialect: "mysql",
+    dialectModule: require('mysql2'),
+})
 
 try {
     sequelize.authenticate();
